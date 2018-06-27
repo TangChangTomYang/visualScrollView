aa7c8a0c6d15c49740248ad1b3272d973f6480ac
yangrui <yangrui@yangruideMini.ap>
2018-06-27 14:25:00 +0800

    s

---
+++ UIScrollView/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  UIScrollView
+//
+//  Created by China Daily on 2018/3/21.
+//  Copyright © 2018年 China Daily. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+
+@end
